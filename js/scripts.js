9324d227fde685ba5d90cf2d19ca8765659bc2c1
Alyssa DiCarlo <alyssa.dicarlo@gmail.com>
2021-06-02 11:38:52 -0700

    fetched quote from chuck norris api, added to modal

--- js/scripts.js
+++ js/scripts.js
@@ -5,10 +5,27 @@ function toggleModal() {
     overlay.classList.toggle('visible');
 }
 
-toggleModal();
-
 const closeModalButton = document.querySelector('#closeModal');
 
 closeModalButton.addEventListener('click', () => {
     toggleModal();
+});
+
+function buildQuote(theQuote) {
+    const modalElement = document.querySelector('#modal p');
+    modalElement.innerText = theQuote;
+    toggleModal();
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetch(
+        'https://api.chucknorris.io/jokes/random?category=dev'
+    ).then((response) => {
+            return response.json();
+    }).then((data) => {
+            buildQuote(data.value);
+    }).catch((error) => {
+            console.error("ERROR: ", error);
+            return error;
+    });
 });
